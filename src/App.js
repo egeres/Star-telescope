49255f88d51243ffe82828c662dd5411575570d3
egeres <rafaleto@gmail.com>
2021-02-27 10:35:32 +0100

    Block design of a header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 import React, { Component } from "react";
 import axios from 'axios';
 
+import anime from 'animejs/lib/anime.es.js';
+import * as eva from 'eva-icons';
+
+
 // import TableContainer from "./TableContainer"
 // https://thewidlarzgroup.com/react-table-7/
 // https://github.com/TheWidlarzGroup/RT7-example/blob/102c5bbfddf9e01e556b84e81de51ef2cef3ba5e/src/App.js
@@ -12,6 +16,44 @@ import axios from 'axios';
 import TableContainer from "./Table_mine";
 // https://blog.logrocket.com/complete-guide-building-smart-data-table-react/
 // https://github.com/learnwithparam/logrocket-smart-table
+
+import { useState, useEffect } from 'react';
+
+// const FunctionalComponent = () => {
+//     useEffect(() => { 
+//         console.log("aaaaasd");
+//         anime({
+//             targets: this,
+//             translateX: 250,
+//             rotate: '1turn',
+//             backgroundColor: '#FFF',
+//             duration: 800
+//           });
+//     }, []);
+//     return <h1>Hello, world</h1>;
+// };
+
+class Animated_bar extends Component
+{
+    constructor(props) {
+        super(props);
+        this.myself = React.createRef();
+    }
+
+    render() {
+        if (this.props.align === "R") { return (<hr ref={this.myself} style={{ marginRight:"0px", }}></hr>); }
+        else { return (<hr ref={this.myself} style={{ marginLeft:"0px", }}></hr>); }
+    }
+
+    componentDidMount() {
+        anime({
+            targets : this.myself.current,
+            width   : ["0%", "100%"],
+            duration: 5000,
+            easing  : "easeOutQuint", // https://easings.net
+        });
+    }
+}
 
 class App extends Component {
 
@@ -26,10 +68,69 @@ class App extends Component {
   render() {
     return (
       <div style={{backgroundColor: 'black', height:"100%"}}>
-        {this.state.starred_count}
+
+        {/* {this.state.starred_count} */}
 
         {/* <TableContainer columns={columns} data={data} /> */}
-        <TableContainer columns={this.columns} data={this.data} />
+        {/* <TableContainer columns={this.columns} data={this.data} /> */}
+
+        <div style={{width:"100%", display:"flex", flexGrow:1, justifyContent:"center"}}>
+            {/* <div style={{backgroundColor:"black", width:"calc(50% - 100px - 10px)"}}><Animated_bar/></div> */}
+
+            <div style={{
+                backgroundColor: "black",
+                width          : "calc(50% - 100px - 10px)",
+                // display        : "table",
+                // verticalAlign  : "middle",
+                // textAlign      : "center",
+                
+                display       : "flex",
+                flexDirection : "row",
+                justifyContent: "left",
+                alignItems    : "center",
+                // alignContent  : "flex-start",
+                // alignContent:"center"
+                
+                // display:flex;
+                // flex-direction: column;
+                // justify-content: center;
+
+            }}>
+                <Animated_bar align="R"/>
+                {/* <div style={{width:"20px", height:"20px", backgroundColor:"blue"}}></div> */}
+            </div>
+
+            {/* <div style={{}}> <Animated_bar/> asd </div> */}
+            <div style={{width:"200px", height:"200px", backgroundColor:"red"}}></div>
+            <div style={{
+                backgroundColor: "black",
+                width          : "calc(50% - 100px - 10px)",
+                // display        : "table",
+                // verticalAlign  : "middle",
+                // textAlign      : "center",
+                
+                display       : "flex",
+                flexDirection : "row",
+                justifyContent: "left",
+                alignItems    : "center",
+                // alignContent  : "flex-start",
+                // alignContent:"center"
+                
+                // display:flex;
+                // flex-direction: column;
+                // justify-content: center;
+
+            }}>
+                <Animated_bar/>
+                {/* <div style={{width:"20px", height:"20px", backgroundColor:"blue"}}></div> */}
+            </div>
+
+            {/* display: table-cell;
+            vertical-align: middle;
+            text-align: center; */}
+
+            {/* <div style={{}}> <Animated_bar/> </div>             */}
+        </div>
 
       </div>
     )
@@ -37,6 +138,8 @@ class App extends Component {
 
     componentWillMount()
     {
+        
+
         this.columns = [
             {
             // // first group - TV Show
@@ -155,9 +258,12 @@ class App extends Component {
         ]
     }
 
-  componentDidMount()
-  {
-  // const got = require('got');
+    componentDidMount()
+    {
+
+        // eva.replace()
+
+    // const got = require('got');
 
     // const stars = (user) =>
     //   got(`https://api.github.com/users/${user}/starred`)
